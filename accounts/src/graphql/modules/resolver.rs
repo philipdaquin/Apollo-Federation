418b09b979fb3d2604f37dd3ea38dc06546983af
philipdaquin <philipdaquin20@gmail.com>
2022-04-27 07:31:39 +0800

    implemented entitiy

--- accounts/src/graphql/modules/resolver.rs
+++ accounts/src/graphql/modules/resolver.rs
@@ -1,4 +1,12 @@
 use super::model::User;
+use diesel::prelude::*;
+use crate::schema::users;
 
-pub fn get_user_by_id() {}
-pub fn get_user_by_username() {}
+
+pub fn get_user_by_id(id: i32, conn: &PgConnection) -> QueryResult<User> {
+    users::table.filter(users::id.eq(id)).first(conn)
+}
+
+pub fn get_all_users(conn: &PgConnection) -> QueryResult<Vec<User>> { 
+    users::table.load(conn)
+}
--- accounts/src/graphql/modules/schema.rs
+++ accounts/src/graphql/modules/schema.rs
@@ -1,6 +1,8 @@
 use async_graphql::*;
 use async_graphql_actix_web::*;
 use chrono::NaiveDateTime;
+use super::{resolver::{get_user_by_id, get_all_users, self}, model::User};
+use crate::graphql::config::get_conn_from_ctx;
 
 
 #[derive(SimpleObject)]
@@ -12,4 +14,33 @@ pub struct UserType {
     pub password: String,
     pub email: String,
     pub joined_at: NaiveDateTime
+}
+
+pub struct UserQuery;
+
+#[Object]
+impl UserQuery { 
+    #[graphql(entity)]
+    async fn find_user_by_id(&self, ctx: &Context<'_>, id: ID) -> Option<UserType> { 
+        let id = id.parse::<i32>().expect("Failed To Parse from String");
+        resolver::get_user_by_id(id, &get_conn_from_ctx(ctx))
+            .ok()
+            .map(|user| UserType::from(&user))
+    }
+}
+
+
+
+impl From<&User> for UserType { 
+    fn from(user: &User) -> Self {
+        Self { 
+            id: user.id.into(),
+            first_name: user.first_name.clone(),
+            last_name: user.last_name.clone(),
+            username: user.username.clone(),
+            password: user.password.clone(),
+            email: user.email.clone(),
+            joined_at: user.joined_at
+        }
+    }
 }
