b363c982c387d01c5ceb93e492bf4d4f2c20ca19
philipdaquin <philipdaquin20@gmail.com>
2022-04-29 08:59:19 +0800

    wait for, docker apollo

--- apollo-gateway/index.js
+++ apollo-gateway/index.js
@@ -15,15 +15,18 @@ const { ApolloGateway, RemoteGraphQLDataSource,
 //         }
 //     }
 // }
+
+
+
 //  Intialise an ApolloGateway instance and pass it 
 //  the supergraph schema 
 const gateway = new ApolloGateway({
     supergraphSdl: new IntrospectAndCompose({
         subgraphs: [
-            { name: 'accounts', url: 'http://localhost:4001/graphql '},
-            { name: 'products', url: 'http://localhost:4002/graphql '},
-            { name: 'reviews', url: 'http://localhost:4003/graphql '},
-            { name: 'inventory', url: 'http://localhost:4004/graphql '},
+            { name: 'accounts', url: 'http://localhost:4001/graphql'},
+            { name: 'products', url: 'http://localhost:4002/graphql'},
+            { name: 'reviews', url: 'http://localhost:4003/graphql'},
+            { name: 'inventory', url: 'http://localhost:4004/graphql'},
         ],
         // buildService({name, url}) {
         //     return new AuthenticatedDataSource({url});
