b85cd47e6b4e6ab5daf9339df1eb67a87f24c1db
philipdaquin <philipdaquin20@gmail.com>
2022-05-02 12:27:34 +0800

    added additional data to the product models and created CRUD

--- products/src/graphql/modules/model.rs
+++ products/src/graphql/modules/model.rs
@@ -1,5 +1,5 @@
 use crate::schema::products;
-
+use chrono::{NaiveDateTime, NaiveDate};
 #[derive(Debug, Clone, PartialEq, Identifiable, Queryable)]
 #[table_name = "products"]
 pub struct Product { 
@@ -7,4 +7,43 @@ pub struct Product {
     pub name: String,
     pub price: Option<i32>, 
     pub weight: Option<i32>,
+    pub category: Option<String>, 
+    pub created_by: Option<i32>,
+    pub tags: Option<String>,
+    pub created_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>,
+    pub description: Option<String>,
+    pub image_url: Option<String>
+}
+
+#[derive(Debug, Clone, AsChangeset, Insertable)]
+#[table_name = "products"]
+pub struct NewProduct { 
+    pub name: String,
+    pub price: Option<i32>, 
+    pub weight: Option<i32>,
+    pub category: Option<String>, 
+    pub created_by: Option<i32>,
+    pub tags: Option<String>,
+    pub created_at: Option<NaiveDateTime>,
+    pub updated_at: Option<NaiveDateTime>,
+    pub description: Option<String>,
+    pub image_url: Option<String>
+}
+
+pub struct ShoppingCart { 
+    id: i32,
+    session_id: i32, 
+    product_id: i32, 
+    quantity: i32, 
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime 
+}
+
+pub struct ShoppingSession { 
+    id: i32, 
+    user_id: i32, 
+    total: i32, 
+    created_at: NaiveDateTime,
+    updated_at: NaiveDateTime
 }
--- products/src/graphql/modules/resolver.rs
+++ products/src/graphql/modules/resolver.rs
@@ -1,5 +1,5 @@
 use diesel::prelude::*;
-use super::model::Product;
+use super::model::{Product, NewProduct};
 use crate::schema::products;
 
 pub fn get_product_by_id(id: i32, conn: &PgConnection) -> QueryResult<Product> { 
@@ -8,4 +8,33 @@ pub fn get_product_by_id(id: i32, conn: &PgConnection) -> QueryResult<Product> {
 
 pub fn get_all_products(conn: &PgConnection) -> QueryResult<Vec<Product>> { 
     products::table.load(conn)
+}
+
+pub fn create_product(new_product: NewProduct, conn: &PgConnection) -> QueryResult<Product> { 
+    diesel::insert_into(products::table)
+        .values(new_product)
+        .get_result::<Product>(conn)
+}
+
+pub fn update_product(
+    product_id: i32, 
+    user_id: i32, 
+    new_product: NewProduct, 
+    conn: &PgConnection
+) -> QueryResult<Product> { 
+    //  Select Table and filter by user_id and find the product_id 
+    diesel::update(
+        products::table
+        .filter(products::created_by.eq(user_id))
+        .find(product_id)
+    )
+    .set(new_product)
+    .get_result::<Product>(conn)
+}
+
+pub fn delete_product(product_id: i32, conn: &PgConnection) -> QueryResult<bool> { 
+    diesel::delete(products::table)
+        .filter(products::id.eq(product_id))
+        .execute(conn)?;
+    Ok(true)
 }
